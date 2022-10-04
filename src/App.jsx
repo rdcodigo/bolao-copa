35e0ed24e62ed35c64a9f23f15b8762790d24835
pbdiro <104860032+rdcodigo@users.noreply.github.com>
2022-10-04 14:06:25 -0300

    Iniciando App

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-red-500">
+      Olá Mundo
+    </div>
+  )
+}
+
+export default App
